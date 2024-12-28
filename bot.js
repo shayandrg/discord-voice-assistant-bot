7709030fba12312a6e345c03c7d649907006a130
shayandrg <shayan.derogarian@gmail.com>
2024-12-28 18:45:09 +0330

    feat: add ping command

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const path = require('path')
-const { Client, GatewayIntentBits } = require('discord.js')
+const { Client, GatewayIntentBits, InteractionType } = require('discord.js')
 const { 
     joinVoiceChannel, 
     createAudioPlayer, 
@@ -30,6 +30,42 @@ const mp3Path = path.join(__dirname, 'voice.mp3')
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        const message = await interaction.reply({ content: 'Let me tell you something...', fetchReply: true });
+        const messageLatency = message.createdTimestamp - interaction.createdTimestamp;
+        const apiLatency = client.ws.ping;
+
+        await interaction.editReply({
+            content: null,
+            embeds: [
+                {
+                    title: `${client.user.username} ✨`,
+                    color: 0xff0000,
+                    fields: [
+                        {
+                            name: 'Message Latency',
+                            value: `\`${messageLatency}ms\``,
+                            inline: true
+                        },
+                        {
+                            name: 'API Ping (WebSocket)',
+                            value: `\`${apiLatency}ms\``,
+                            inline: true
+                        }
+                    ],
+                    footer: {
+                        text: `📡 ${interaction.user.username}™`,
+                        icon_url: interaction.user.displayAvatarURL()
+                    }
+                }
+            ]
+        })
+    }
+});
 
 client.on('voiceStateUpdate', (oldState, newState) => {
     const channelId = newState.channel?.id
---
+++ deploy-commands.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const { REST, Routes } = require('discord.js')
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!'
+    }
+]
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.')
+
+        await rest.put(
+            Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID),
+            { body: commands }
+        )
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error)
+    }
+})();
